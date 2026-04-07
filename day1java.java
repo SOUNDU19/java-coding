0009c86ce34838eebe1c145747ca707ab16a4dbf
Soundarya <ssoundarya090@gmail.com>
2026-04-07 21:37:16 +0530

    day1 commit

--- day1java.java
+++ day1java.java
@@ -9,20 +9,43 @@
         
 //     }
 // }
+// public class day1java{
+//     int instance_var = 10;
+//     static String var = "hrllo";
+
+//     public void method1(){
+//         int loc_var = 20;
+//         System.out.println("instance variable is: " + instance_var);
+//         System.out.println("static variable is :"+var);
+//         System.out.println("local varaible:"+loc_var);
+//     }
+//     public static void main(String[] args){
+//           day1java obj = new day1java();
+//           obj.method1();
+
+//           System.out.println("ststic variables:"+ day1java.var);
+//         }
+// }
+
+//datatypes
+
 public class day1java{
-    int instance_var = 10;
-    static String var = "hrllo";
-
-    public void method1(){
-        int loc_var = 20;
-        System.out.println("instance variable is: " + instance_var);
-        System.out.println("static variable is :"+var);
-        System.out.println("local varaible:"+loc_var);
+    public static void main(String[]args){
+        int a = 10;
+        byte b= 20;
+        short c =30;
+        long d= 2000000000L;
+        float e = 3.14f;
+        double g= 3.141592653589793;
+        char f = 'e';
+        boolean q = true;
+        System.out.println("int value:"+ a);
+        System.out.println("byte value:"+b);
+        System.out.println("short value:"+c);
+        System.out.println("long value:"+d);
+        System.out.println("float value:"+e);
+        System.out.println("double value:"+g);
+        System.out.println("char value:"+f);
+        System.out.println("boolean value:"+q);
     }
-    public static void main(String[] args){
-          day1java obj = new day1java();
-          obj.method1();
-
-          System.out.println("ststic variables:"+ day1java.var);
-        }
 }
