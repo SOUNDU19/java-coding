1356b022488cd15b5f5ef8f463b872d07ade19df
Soundarya <ssoundarya090@gmail.com>
2026-04-07 21:24:43 +0530

    done

--- day1java.java
+++ day1java.java
@@ -1,11 +1,28 @@
-public class day1java{
-    public static void main(String[] args){
-        // System.out.println("hello world");
-        int [] arr = { 1,2 ,3,4,5};
-        for(int i = 0;i < arr.length; i++){
-            System.out.print (arr[i]+" ");
-        }
+// public class day1java{
+//     public static void main(String[] args){
+//         // System.out.println("hello world");
+//         int [] arr = { 1,2 ,3,4,5};
+//         for(int i = 0;i < arr.length; i++){
+//             System.out.print (arr[i]+" ");
+//         }
       
         
+//     }
+// }
+public class day1java{
+    int instance_var = 10;
+    static String var = "hrllo";
+
+    public void method1(){
+        int loc_var = 20;
+        System.out.println("instance variable is: " + instance_var);
+        System.out.println("static variable is :"+var);
+        System.out.println("local varaible:"+loc_var);
     }
+    public static void main(String[] args){
+          day1java obj = new day1java();
+          obj.method1();
+
+          System.out.println("ststic variables:"+ day1java.var);
+        }
 }
