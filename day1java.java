ac52553a4180186d7c4cd7be11f29e25554c7268
Soundarya <ssoundarya090@gmail.com>
2026-04-07 19:34:58 +0530

    done

--- day1java.java
+++ day1java.java
@@ -1,5 +1,11 @@
 public class day1java{
     public static void main(String[] args){
-        System.out.println("hello world");
+        // System.out.println("hello world");
+        int [] arr = { 1,2 ,3,4,5};
+        for(int i = 0;i < arr.length; i++){
+            System.out.print (arr[i]+" ");
+        }
+      
+        
     }
 }
