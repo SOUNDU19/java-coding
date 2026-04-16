3ffe21897f84ebcc9cdc1ffe378f76dc68940dc1
Soundarya H S <ssoundarya090@gmail.com>
2026-04-16 08:52:41 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,38 @@
+package Java;
+
+public class task1 {
+
+	public static void main(String[] args) {
+		var alarm = true;
+		var bath = true;
+		var breakfast = true;
+		var learn = true;
+		var homework = true;
+		var hungry = true;
+		var sleep = true;
+		
+		if (alarm) {
+			System.out.println("Wake up");
+		}
+		else {
+			System.out.println("Not Waking up");
+		}
+		System.out.println("GOT TO BAth");
+		
+		if(bath) {
+			System.out.println("take bath");
+		}
+		else {
+			System.out.println("Not taking abath");
+		}
+		System.out.println("GOT braeakfast");
+		if(breakfast) {
+			System.out.println("eat breakfasr");
+		}
+		else {
+			System.out.println("Not eating breakfast");
+		}
+		System.out.println("do homework");
+		}
+
+}
